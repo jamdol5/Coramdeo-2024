0b6e86228aa3605a45c13af6d8ed35f9510b3bbd
jamdol5 <78344248+jamdol5@users.noreply.github.com>
2024-10-02 20:59:34 -0700

    Update 지출.py

--- pages/지출.py
+++ pages/지출.py
@@ -3,6 +3,35 @@ import pandas as pd
 from utils.data_manager import DataManager
 import json
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if (
+            st.session_state["username"] == st.secrets["credentials"]["username"]
+            and st.session_state["password"] == st.secrets["credentials"]["password"]
+        ):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show inputs for username + password.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input("Password", type="password", on_change=password_entered, key="password")
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input("Username", on_change=password_entered, key="username")
+        st.text_input("Password", type="password", on_change=password_entered, key="password")
+        st.error("😕 User not known or password incorrect")
+        return False
+    else:
+        # Password correct
+        return True
+
 # Fetch the data from Streamlit secrets
 data = json.loads(st.secrets["accounting_data"]["data"])
 
